83bd8d3da2cbef46115fac0d2390e046748f5a64
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2024-01-02 14:08:10 -0600

    displays info for each pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@
 let pokemonList = [
   {
     name: 'Charizard',
-    height: 1.7,
+    height: 2.1,
     type: ['fire', 'flying']
   },
   {
@@ -13,7 +13,21 @@ let pokemonList = [
   },
   {
       name: 'Pikachu',
-      height: .4,
+      height: .3,
       type: ['electric']
   }
 ];
+
+// displays a pokemon's name, types, and height
+// also adds extra descriptors for pokemon above 2 height or below .35 height
+
+for (let i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ' ' + pokemonList[i].type);
+  if (pokemonList[i].height > 2) {
+    document.write('Height: ' + pokemonList[i].height + ' Wow! That\'s large!')
+  } else if (pokemonList[i].height < .35) {
+    document.write('Height: ' + pokemonList[i].height + ' Aw, it\'s tiny!')
+  } else {
+    document.write('Height: ' + pokemonList[i].height)
+  }
+};
