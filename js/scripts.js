77aa7b08d038f40f8ecc06035ce7b7844a854a75
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2024-01-01 19:26:26 -0600

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+// height is in m
+
+let pokemonList = [
+  {
+    name: 'Charizard',
+    height: 1.7,
+    type: ['fire', 'flying']
+  },
+  {
+    name: 'Squirtle',
+    height: .5,
+    type: ['water']
+  },
+  {
+      name: 'Pikachu',
+      height: .4,
+      type: ['electric']
+  }
+];
