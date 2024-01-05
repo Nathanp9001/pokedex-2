5af81a588ec8cc4a18a512c21981f88838b3ceff
Nathanp9001 <109004356+Nathanp9001@users.noreply.github.com>
2024-01-04 21:15:13 -0600

    clean up code and put pokemon list in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -12,22 +12,33 @@ let pokemonList = [
     type: ['water']
   },
   {
-      name: 'Pikachu',
-      height: .3,
-      type: ['electric']
+    name: 'Pikachu',
+    height: .3,
+    type: ['electric']
   }
 ];
 
+let pokemonRepository = (function() {
+  let pokemonList = [];
+  
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {getAll: getAll};
+})();
+
 // displays a pokemon's name, types, and height
 // also adds extra descriptors for pokemon above 2 height or below .35 height
+function printArrayDetails(pokemon){
+    document.write('<p>' + pokemon.name + '</p>' + '<p>' + pokemon.type + '</p>');
+    if (pokemon.height > 2) {
+      document.write('<p>' + 'Height: ' + pokemon.height + ' Wow! That\'s large!' + '</p>')
+    } else if (pokemon.height < .35) {
+      document.write('<p>' + 'Height: ' + pokemon.height + ' Aw, it\'s tiny!' + '</p>')
+    } else {
+      document.write('<p>' + 'Height: ' + pokemon.height + '</p>')
+    }
+  };
 
-for (let i = 0; i < pokemonList.length; i++) {
-  document.write(pokemonList[i].name + ' ' + pokemonList[i].type);
-  if (pokemonList[i].height > 2) {
-    document.write('Height: ' + pokemonList[i].height + ' Wow! That\'s large!')
-  } else if (pokemonList[i].height < .35) {
-    document.write('Height: ' + pokemonList[i].height + ' Aw, it\'s tiny!')
-  } else {
-    document.write('Height: ' + pokemonList[i].height)
-  }
-};
+pokemonList.forEach(printArrayDetails);
